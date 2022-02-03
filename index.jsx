c5b37a974e7d15e5b3ba9f010c4831ace2e69d2f
julian <Joolean14>
2022-02-02 20:10:17 -0500

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+function App() {
+  return <h1>Your React Playground is ready!</h1>;
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
